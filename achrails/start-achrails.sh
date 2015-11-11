27f6d35e10ee6810c6f9f0884dcd7612d7ed4abf
bqqbarbhg <samuli.1995@hotmail.com>
2015-11-11 15:21:32 +0200

    Move startup to bash script

---
+++ achrails/start-achrails.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+# Setup database
+rake db:create
+rake db:migrate
+
+# Precompile assets
+bundle exec rake assets:precompile
+
+# Start the server
+rails server -b 0.0.0.0 -p 3000
+
