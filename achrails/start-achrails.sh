a2e88b060c45c43cd4ff263c427390833a0e374e
bqqbarbhg <samuli.1995@hotmail.com>
2016-03-18 11:06:45 +0200

    Update to v0.6.1

--- achrails/start-achrails.sh
+++ achrails/start-achrails.sh
@@ -7,6 +7,9 @@ rake db:migrate
 # Precompile assets
 bundle exec rake assets:precompile
 
+# Purge in the background
+./purge-sessions.sh &
+
 # Start the server
 exec rails server -b 0.0.0.0 -p 3000
 
